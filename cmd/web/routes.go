6ded2388c53b48069d1400f94d84a6cbfb70e6f7
NathanielRand <nathanieljrand@gmail.com>
2021-02-14 00:53:16 +0000

    test if ide is stopping js

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,8 +16,13 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
+	// Routes
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	// Serve static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
