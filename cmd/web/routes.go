c4ea58f1b935547f710e0f2c63638b24aa97357a
NathanielRand <nathanieljrand@gmail.com>
2021-02-17 03:37:47 +0000

    Implement ajax for form post

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -36,7 +36,7 @@ func routes(app *config.AppConfig) http.Handler {
 	// Routes - Search Availability
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
-	mux.Get("/search-availability-json", handlers.Repo.AvailabilityJSON)
+	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
 
 	// Serve static files
 	fileServer := http.FileServer(http.Dir("./static/"))
